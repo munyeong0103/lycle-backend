65eed88070f0c650e8bc3790b1c38182ea293a9e
eunju <dmswn0261@ajou.ac.kr>
2022-12-03 20:44:11 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/opt/lycleCodeDeploy
+REPOSITORY=/lycle-backend
 cd $REPOSITORY
 
 APP_NAME=lycle-backend-0.0.1-SNAPSHOT.jar
@@ -14,9 +14,9 @@ then
   echo "> 종료할것 없음."
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill $CURRENT_PID
   sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $APP_NAME &
