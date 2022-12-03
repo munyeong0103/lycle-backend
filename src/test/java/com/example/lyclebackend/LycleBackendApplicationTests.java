6ae2dc9a8e656aeced0c426f62ac225d7503b24e
eunju <dmswn0261@ajou.ac.kr>
2022-12-03 19:20:32 +0900

    deploy

--- src/test/java/com/example/lyclebackend/LycleBackendApplicationTests.java
+++ src/test/java/com/example/lyclebackend/LycleBackendApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.lyclebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class LycleBackendApplicationTests {
+@ActiveProfiles("test")
+public class LycleBackendApplicationTests {
 
     @Test
     void contextLoads() {
