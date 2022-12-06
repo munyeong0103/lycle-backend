48ed8481355895e518aed5ff36e38cfb0ad9b88a
eunju <dmswn0261@ajou.ac.kr>
2022-12-06 15:24:59 +0900

    check

--- src/test/java/com/example/lyclebackend/LycleBackendApplicationTests.java
+++ src/test/java/com/example/lyclebackend/LycleBackendApplicationTests.java
@@ -1,16 +1,18 @@
 package com.example.lyclebackend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class LycleBackendApplicationTests {
 
     @Test
     void contextLoads() {
-
     }
 
 }
