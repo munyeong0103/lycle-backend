49b38fdaceeb0fd87ae27a36f466854ebab3250f
eunju <dmswn0261@ajou.ac.kr>
2022-12-06 15:39:46 +0900

    check

--- deploy.sh
+++ deploy.sh
@@ -30,6 +30,3 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행"
 nohup java -jar $JAR_FILE &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다."
